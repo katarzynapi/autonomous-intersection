2c490bc9ef9197b98d555758c541581f909c415a
kaliope314 <katarzynka314@gmail.com>
2019-12-06 00:10:41 +0100

    version 4.0
    jeżdżenie po ścieżkach

--- autonomousIntersection.py
+++ autonomousIntersection.py
@@ -22,6 +22,11 @@ class IfBorder(Enum):
     INPUT = 1
     OUTPUT = 2
 
+class PathType(Enum):
+    MAIN = 1
+    ALTERNATIVE = 2
+    OPPOSITE = 3
+
 #_________________________________________________________________________
 # map
 
@@ -47,6 +52,20 @@ class Cell:
     def add_obstacle(obstacle):
         self.obstacles.append(obstacle)
 
+    def find_fwd_and_bwd_path(self):
+        fwd_path = []
+        bwd_path = []
+        curr_cell = self
+        while curr_cell.f_neighbour is not None:
+            fwd_path.append(curr_cell.f_neighbour)
+            curr_cell = curr_cell.f_neighbour
+        curr_cell = self
+        bwd_path.append(curr_cell)
+        while curr_cell.b_neighbour is not None:
+            bwd_path.append(curr_cell.b_neighbour)
+            curr_cell = curr_cell.b_neighbour
+        return (fwd_path, bwd_path)
+
 class Grid:
     def __init__(self, min_x_coords, min_y_coords, max_x_coords, max_y_coords):
         self.coords_range = min_x_coords, min_y_coords, max_x_coords, max_y_coords
@@ -62,6 +81,12 @@ class Grid:
     
     def getCoordsRange(self):
         return self.coords_range
+
+class Path:
+    def __init__(self, path_type, cell_list):
+        self.type = path_type
+        self.cells = cell_list
+        
 		
 class Coordinates:
     def __init__(self, x_coords=0.0, y_coords=0.0):
@@ -110,6 +135,7 @@ class Crossing_Entering(Obstacle):
 
 class GeneralAgent:
     _counter = -1
+    far_away = 10000
     vel_table = np.array([np.arange(1,11,1),np.arange(2,22,2),np.arange(7.2,79.2,7.2)])
     def __init__(self):
         GeneralAgent._counter += 1
@@ -124,7 +150,8 @@ class GeneralAgent:
         self.visibility = 0.0           # z jakiej odległości widać agenta
         self.field_of_view = 80.0        # na jaką odległość widzi agent
         self.destination = None
-        self.path = []                  # pierwsza komórka w path to któryś z przednich sąsiadów head
+        self.all_paths = []
+        self.path = None                  # pierwsza komórka w path to któryś z przednich sąsiadów head
         self.obstacles = []
         self.how_many_cells_forward = 0
         self.new_velocity = 0.0
@@ -139,6 +166,9 @@ class GeneralAgent:
             cell_to_add.agent = self
             self.location.insert(0,cell_to_add)
 
+    def ascribe_paths(self, paths_list):
+        self.all_paths = paths_list
+        self.path = next((p for p in paths_list if p.type == PathType.MAIN), None)
 
     @property
     def head(self):
@@ -175,14 +205,35 @@ class GeneralAgent:
         else:
             return False
 
-    def find_path():
-        #TODO
-        # algorytm wyznaczający trasę w oparciu o lokalizację i cel
-        pass      
+    def alternative_route_exists(self):
+        for p in self.all_paths:
+            if (p.type == PathType.ALTERNATIVE and (self.head.r_neighbour in p.cells or self.head.r_neighbour in p.cells)):
+                return True
+        return False
+
+    def opposite_route_exists(self):
+        for p in self.all_paths:
+            if p.type == PathType.OPPOSITE and (self.head.r_neighbour in p.cells or self.head.r_neighbour in p.cells):
+                return True
+        return False
+
+    def can_safely_change_to_other_path(self, other_path_fwd, other_path_bwd):
+        #gdyby się okazało, że to wszystko wali błędami, to przerobimy na zwykłą pętlę for
+        (prev_dist, prev_vehicle) = next(((len(other_path_bwd)-1-i, x.agent) for i, x in other_path_bwd[::-1] if x.agent is not None), (None, None))
+        (next_dist, next_vehicle) = next(((i+1, x.agent) for i, x in other_path_fwd if x.agent is not None), (None, None))
+        if prev_vehicle is None:
+            prev_safe = True
+        else:
+            prev_safe = (prev_vehicle.stopping_dist(-2) + prev_dist - self.how_many_cells() > self.stopping_dist(-2))
+        if next_vehicle is None:
+            next_safe = True
+        else:
+            next_safe = (self.stopping_dist(-2) + next_distance > next_vehicle.stopping_dist(-2))
+        return (next_safe and prev_safe)
 
     def visible_path(self):
         # powinno zwracać ten fragment wyznaczonej trasy, który agent widzi
-        return self.path
+        return self.path.cells
         #return [c for c in self.path if self.location[0].coords.calculate_distance(c.coords) > self.field_of_view]
 
     def scan_for_obstacles(self):
@@ -192,13 +243,65 @@ class GeneralAgent:
         self.obstacles = current_obstacles
 
     def calc_path_distance(self, obstacle):
-        for c in self.visible_path():
-            if x in c.obstacles: # co to jest x
-                return index(c)+1
+        for i, c in enumerate(self.visible_path()):
+            if obstacle in c.obstacles or obstacle == c.agent:
+                return i+1
         return 0
 
     def find_first_agent_on_path(self):
         return next((x.agent for x in self.visible_path() if x.agent is not None), None)
+
+    def find_nearest_lights(self):
+        return next((obs for obs in self.obstacles if isinstance(obj, Lights)), None)
+
+    def find_nearest_blockade(self):
+        return next((obs for obs in self.obstacles if isinstance(obj, Blockade)), None)
+
+
+    def update_route(self):
+        if self.path.type == PathType.OPPOSITE:
+            neighbour_main = self.head.neighbour_r
+            main_path = next((p for p in self.all_paths if p.type == PathType.MAIN), None)
+            start = main_path.index(neighbour_main)
+            main_fwd = main_path[start+1:]
+            main_bwd = main_path[:start+1]
+            if can_safely_change_to_other_path(main_fwd, main_bwd):
+                self.path.type = PathType.MAIN
+                self.path.cells = main_bwd
+        else:
+            if self.path.type == PathType.ALTERNATIVE and self.path.cells[-1].if_border == IfBorder.NOT and len(self.path.cells) < self.stopping_dist(-1) + 10:
+                neighbour_main = self.head.neighbour_r
+                main_path = next((p for p in self.all_paths if p.type == PathType.MAIN), None)
+                start = main_path.index(neighbour_main)
+                main_fwd = main_path[start+1:]
+                main_bwd = main_path[:start+1]
+                if can_safely_change_to_other_path(main_fwd, main_bwd):
+                    self.path.type = PathType.MAIN
+                    self.path.cells = main_bwd
+            else:
+                blockade = self.find_nearest_blockade()
+                if blockade is not None:
+                    if self.path.type == PathType.ALTERNATIVE:
+                        neighbour_main = self.head.neighbour_r if self.head.neighbour_r is not None else self.head.neighbour_l
+                        main_path = next((p for p in self.all_paths if p.type == PathType.MAIN), None)
+                        start = main_path.index(neighbour_main)
+                        main_fwd = main_path[start+1:]
+                        main_bwd = main_path[:start+1]
+                        if can_safely_change_to_other_path(main_fwd, main_bwd):
+                            self.path.type = PathType.MAIN
+                            self.path.cells = main_bwd
+                    else:
+                        avail_alt = [p for p in self.all_paths if p.type == PathType.ALTERNATIVE and (self.head.neighbour_r in p.cells or self.head.neighbour_l in p.cells)]
+                        for p in avail_path:
+                            neighbour_alt = self.head.neighbour_r if self.head.neighbour_r in p else self.head.neighbour_l
+                            start = p.index(neighbour_alt)
+                            alt_fwd = p[start+1:]
+                            alt_bwd = p[:start+1]
+                            if can_safely_change_to_other_path(alt_fwd, alt_bwd):
+                                self.path.type = PathType.ALTERNATIVE
+                                self.path.cells = alt_fwd
+        self.scan_for_obstacles()
+
 
     def compute_new_location(self):
         self.how_many_cells_forward = int(round(self.velocity))
@@ -212,14 +315,20 @@ class GeneralAgent:
     #gdzieś w tej funkcji radośnie przesądzam i trochę hardcoduję, że opóźnienie może być tylko -1 lub -2
     def compute_new_acceleration(self):
         agent_on_path = self.find_first_agent_on_path()
-        lights = next((obs for obs in self.obstacles if isinstance(obj, Lights)), None)
-        l_dist = 10000           #jakaś losowa duża liczba - oznacza, że świateł/agenta nie ma (w założeniu nic nie jest tak daleko)
-        a_dist = 10000
+        lights = self.find_nearest_lights()
+        blockade = self.find_nearest_blockade()
+        
+        l_dist = self.far_away           #jakaś losowa duża liczba - oznacza, że świateł/agenta nie ma (w założeniu nic nie jest tak daleko)
+        a_dist = self.far_away
+        b_dist = self.far_away
+        
         if lights is not None and lights.colour == Colour.RED:
             l_dist = self.calc_path_distance(lights)
         if agent_on_path is not None:
-            a_dist = self.calc_path_distance(agent_on_path) + self.agent_on_path.stopping_dist(-2)
-        dist = min(a_dist, l_dist)
+            a_dist = self.calc_path_distance(agent_on_path) + agent_on_path.stopping_dist(-2)
+        if blockade is not None:
+            b_dist = self.calc_path_distance(blockade)
+        dist = min(a_dist, l_dist, b_dist)
         if dist > self.stopping_dist(-1)*2:
             self.acceleration = 1 if self.velocity < 7 else 0
         #elif dist > stopping_dist(-1)*1.5:
@@ -238,7 +347,7 @@ class GeneralAgent:
         # auto wjeżdża na tyle komórek path, ile wyznaczono na podstawie prędkości
         vel_len_diff = self.how_many_cells_forward - self.length
         start = vel_len_diff if vel_len_diff >= 0 else 0
-        new_car_part = self.path[int(start):int(self.how_many_cells_forward)]
+        new_car_part = self.path.cells[int(start):int(self.how_many_cells_forward)]
         new_car_part.reverse()
         # dodajemy agenta dla nowych komórek
         for cell in new_car_part:
@@ -251,7 +360,7 @@ class GeneralAgent:
         # nowe położenie
         self.location = new_car_part + old_car_part
         # ucinamy z path to, na co wjechało auto
-        self.path = self.path[self.how_many_cells_forward:]
+        self.path.cells = self.path.cells[self.how_many_cells_forward:]
 
 
     #aby wszyscy ruszali się jednocześnie, najpierw trzeba policzyć nowe parametry, a jak wszystkie będą policzone, to je przypisać. Dlatego dwie metody (wzorowałam się na SimultaneousActivation z mesa)
@@ -259,8 +368,7 @@ class GeneralAgent:
     def step(self):
         pass
         self.scan_for_obstacles()
-        #if any(isinstance(x, Blockade) for x in self.obstacles):
-            #self.find_path()
+        self.update_route()
         self.compute_new_location()
         self.compute_new_velocity()
         self.compute_new_acceleration()
---
+++ main_0612.py
@@ -0,0 +1,151 @@
+import cv2
+import numpy as np
+import autonomousIntersection as autointer
+import mapDrawing as mapdraw
+import random
+import dill
+import sys
+sys.setrecursionlimit(10000)
+
+'''def find_nearest_pixel(pixels, init_pixel):
+    INIT_PIXEL_COORDS, INIT_PIXEL_ID = init_pixel
+    distances = np.sqrt((INIT_PIXEL_COORDS[0] - pixels[:][0]) ** 2 + (INIT_PIXEL_COORDS[1] - pixels[:][1]) ** 2)
+    nearest_pixel = np.argmin(distances)
+    return pixels[nearest_pixel], nearest_pixel'''
+
+# import map image
+img = cv2.imread('Xcross_cell_colors.png')
+clean_img = cv2.imread('Xcross_cell_colors.png')
+height, width, channels = img.shape
+
+#create model for imported map
+model = autointer.IntersectionModel(0, 0, width, height)
+model.image = img
+cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', 1500, 735)
+
+orange = [87, 189, 254]
+green = [87, 193, 135]
+nw = [10, 29, 148]
+ne = [12, 56, 143]
+sw = [33, 128, 254]
+se = [26, 103, 223]
+blue = [222, 179, 6]
+yellow = [94, 247, 254]
+counter = 0
+'''
+# add cells for road fragment (based on centers of drawn circles = cells)
+# green cells
+for i, row in enumerate(img):
+    for j, col in enumerate(row):
+        if np.array_equal(green, col) or np.array_equal(nw, col) or np.array_equal(sw, col):
+            print(counter)
+            counter+=1
+            cv2.circle(clean_img, (j, i), 3, (0, 0, 0), 2)
+            cv2.imshow('image', clean_img)
+            cv2.waitKey(1)
+            model.grid.addCell(j, i, autointer.CellType.ROAD)
+# orange cells
+print("orange")
+for i, row in enumerate(img):
+    for j, col in enumerate(row):
+        if np.array_equal(orange, col) or np.array_equal(ne, col) or np.array_equal(se, col):
+            print(counter)
+            counter+=1
+            cv2.circle(clean_img, (j, i), 3, (0, 0, 0), 2)
+            cv2.imshow('image', clean_img)
+            cv2.waitKey(1)
+            model.grid.addCell(j, i, autointer.CellType.ROAD)
+print("green")
+
+cells_number = len(model.grid.cells)
+model.grid.cells[0].if_border = autointer.IfBorder.INPUT
+model.grid.cells[int(cells_number/2)-1].if_border = autointer.IfBorder.OUTPUT
+model.grid.cells[int(cells_number/2)].if_border = autointer.IfBorder.INPUT
+model.grid.cells[-1].if_border = autointer.IfBorder.OUTPUT
+
+# add cells' f and b neighbours
+for i, cell in enumerate(model.grid.cells):
+    if cell.if_border == autointer.IfBorder.INPUT:
+        cell.f_neighbour = model.grid.cells[i+1]
+    elif cell.if_border == autointer.IfBorder.NOT:
+        cell.f_neighbour = model.grid.cells[i+1]
+        cell.b_neighbour = model.grid.cells[i-1]
+    elif cell.if_border == autointer.IfBorder.OUTPUT:
+        cell.b_neighbour = model.grid.cells[i-1]
+
+# add cells' l and r neighbours
+for o_cell, g_cell in zip (model.grid.cells[:92], model.grid.cells[92:]):
+    o_cell.l_neighbour = g_cell
+    g_cell.r_neighbour = o_cell
+
+# add to list of inputs and iutputs
+for cell in model.grid.cells:
+    if cell.if_border == autointer.IfBorder.INPUT:
+        model.grid.inputs.append(cell)
+    if cell.if_border == autointer.IfBorder.OUTPUT:
+        model.grid.outputs.append(cell)
+'''
+#dill.dump( model, open( "model1.d", "wb" ) )
+model = dill.load( open( "model1.d", "rb" ) )
+#_______________________________________________________________________
+'''for i, cell in enumerate(model.grid.cells):
+    print(i)
+    print(cell.getCoords())
+    print(cell.if_border)
+    print("forward")
+    if cell.f_neighbour:
+        print(cell.f_neighbour.getCoords())
+    print("backward")
+    if cell.b_neighbour:
+        print(cell.b_neighbour.getCoords())
+    print("left")
+    if cell.l_neighbour:
+        print(cell.l_neighbour.getCoords())
+    print("right")
+    if cell.r_neighbour:
+        print(cell.r_neighbour.getCoords())
+    print("")
+'''
+#create paths
+for inp in model.grid.inputs:
+    #add path with going straight
+    current_cell = inp
+    straight_path = [current_cell]
+    while current_cell.if_border != autointer.IfBorder.OUTPUT:
+        straight_path.append(current_cell.f_neighbour)
+        current_cell = current_cell.f_neighbour
+    model.paths[(inp, current_cell)] = autointer.Path(autointer.PathType.MAIN, straight_path)
+model.paths[(inp, current_cell)].type = autointer.PathType.ALTERNATIVE
+
+# simulate agent movement along the road
+destination_reached = False
+i = 0
+#model.lights.append(autointer.Lights())
+#model.lights[0].location = model.grid.cells[20]
+while destination_reached == False:
+    #print(model.lights[0].colour)
+    if i%100==0:
+        # add default agent to model
+        model.generateAgent()
+        model.agents[-1].ascribe_paths(list(model.paths.values()))
+        model.agents[-1].place_on_grid(model.grid.inputs[0])
+        #model.agents[-1].path.extend(model.paths[(model.grid.inputs[0], model.grid.outputs[0])][5:])
+        model.agents[-1].destination = model.grid.outputs[0]
+    model.image = clean_img.copy()
+    model.showAgentMovement(0, 8)
+    model.step()
+    destination_reached = model.agents[0].reached_destination()
+    i+=1
+
+cv2.destroyAllWindows()
+
+
+'''pixels = []
+for i in model.grid.cells:
+    pixels.add((i.coords.getCoordinates(), i))
+init_pixel = pixels[-1]
+while pixels:
+    nearest_pixel, idx = find_nearest_pixel(pixels, init_pixel)
+    init_pixel = nearest_pixel
+    del pixels[idx]'''
